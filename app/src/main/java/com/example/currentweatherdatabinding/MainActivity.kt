6c00ad405ff6503abfe0d6fdd8b68ccca7147878
sulyvahn <kurapatka.kudah@gmail.com>
2023-03-17 11:31:00 +0800

    Better coroutines work

--- app/src/main/java/com/example/currentweatherdatabinding/MainActivity.kt
+++ app/src/main/java/com/example/currentweatherdatabinding/MainActivity.kt
@@ -11,40 +11,40 @@ import com.google.gson.Gson
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.InputStream
 import java.net.URL
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
-    // enum class DIRECTION {
-    //     EAST, WEST, NORTH, NORTH_EAST, NORTH_WEST, SOUTH, SOUTH_EAST, SOUTH_WEST
-    // }
-    // enum class CLOUDS {
-    //     SUN, RAIN, PARTLY, CLOUDY, SNOW, THUNDER
-    // }
     val weather = ArrayList<WeatherData> ()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        loadFromResources()
-
         val rv = findViewById<RecyclerView>(R.id.RecyclerView)
         val layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
-
         rv.layoutManager = layoutManager
         val adapter = WeatherAdapter(weather)
         rv.adapter = adapter
-
+        GlobalScope.launch(Dispatchers.Main) {
+            loadFromResources()
+            Log.d("MY_TAG", "Data loaded")
+            adapter.notifyDataSetChanged()
+        }
         val btn = findViewById<Button>(R.id.searchWeather)
         val searchField = findViewById<EditText>(R.id.searchField)
         btn.setOnClickListener {
             val city = searchField.text.toString()
-            var ready = false
-            GlobalScope.launch (Dispatchers.IO) {
+            GlobalScope.launch(Dispatchers.Main) {
                 Log.d("MY_TAG", "Loading $city...")
-                val weatherSingle = loadWeather(city)
+
+                val weatherSingle = try {
+                    loadWeather(city)
+                } catch (e: java.lang.Exception) {
+                    Log.e("MY_TAG", e.toString())
+                    null
+                }
                 weatherSingle?.let {
                     for (item in weather) {
                         if (item.name == city) {
@@ -52,58 +52,41 @@ class MainActivity : AppCompatActivity() {
                             return@let
                         }
                     }
-                    Log.d("MY_TAG", "City $city added")
                     weather.add(0, it)
+                    Log.d("MY_TAG", "City $city added")
+                    adapter.notifyItemInserted(0)
+                    layoutManager.scrollToPosition(0)
                 }
-                ready = true
-            }
-            while (!ready) {
-                // по какой-то причине без нормального тела цикл становится бесконечным
-                if (ready) {
-                    Log.d("MY_TAG", ready.toString())
-                }
-                continue
-            }
-
-            if (weather[0].name == city) {
-                adapter.notifyItemInserted(0)
-                layoutManager.scrollToPosition(0)
             }
             searchField.setText("")
         }
     }
 
-    fun loadWeather(city: String): WeatherData? {
-        val API_KEY = getString(R.string.api_key)
-        val weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$API_KEY&units=metric";
-        return try {
+    suspend fun loadWeather(city: String): WeatherData? {
+        return withContext(Dispatchers.IO) {
+            val API_KEY = getString(R.string.api_key)
+            val weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$API_KEY&units=metric";
             val stream = URL(weatherURL).getContent() as InputStream
             val jsonData = Scanner(stream).nextLine()
             val gson = Gson()
             gson.fromJson(jsonData, WeatherData::class.java)
-        } catch (e: java.lang.Exception) {
-            Log.e("MY_TAG", e.toString())
-            null
         }
+
     }
 
-    fun loadFromResources() {
+    suspend fun loadFromResources() {
         val cities = resources.getStringArray(R.array.cities)
-        var cnt = 0 // считаю количество полученных городов, потому что
-        // не умею ждать окончания треда :)
-
-        for (city in cities) {
-            GlobalScope.launch (Dispatchers.IO) {
-                val weatherSingle = loadWeather(city)
-                cnt++
-                weatherSingle?.let {
-                    weather.add(it)
-                    Log.d("MY_TAG", "Loaded $city")
-                }
+            for (city in cities) {
+                    val weatherSingle = try {
+                        loadWeather(city)
+                    } catch (e: java.lang.Exception) {
+                        Log.e("MY_TAG", e.toString())
+                        null
+                    }
+                    weatherSingle?.let {
+                        weather.add(it)
+                        Log.d("MY_TAG", "Loaded $city")
+                    }
             }
-        }
-        while (cnt < cities.size) {
-            continue
-        }
     }
 }
